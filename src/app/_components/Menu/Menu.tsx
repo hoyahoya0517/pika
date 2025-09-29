bc8c19a1fc5e90d493fa320e842a51c031305500
hoyahoya0517 <hoyahoya0517@naver/com>
2025-09-29 16:55:50 +0900

    ver1.9

--- src/app/_components/Menu/Menu.tsx
+++ src/app/_components/Menu/Menu.tsx
@@ -1,35 +1,52 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Menu.module.css";
 import { VscClose } from "react-icons/vsc";
 import { motion } from "motion/react";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Menu() {
+  const [isFirstPage, setIsFirstPage] = useState(true);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const router = useRouter();
+  const pathname = usePathname();
+  useEffect(() => {
+    if (pathname === "/") {
+      setIsFirstPage(true);
+    } else {
+      setIsFirstPage(false);
+    }
+  }, [pathname]);
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [pathname]);
   return (
     <>
-      {!isMenuOpen ? (
-        <motion.div
-          whileHover={{
-            scale: 0.9,
-            transition: { duration: 0.3, type: "spring", ease: "easeInOut" },
-          }}
-          onClick={() => setIsMenuOpen(true)}
-          className={styles.menu}
-        ></motion.div>
-      ) : (
-        <div className={styles.menuClose}>
-          <VscClose
-            color="#000000"
-            size={32}
-            onClick={() => setIsMenuOpen(false)}
-          />
-        </div>
-      )}
+      {!isFirstPage ? (
+        !isMenuOpen ? (
+          <motion.div
+            whileHover={{
+              scale: 0.9,
+              transition: { duration: 0.3, type: "spring", ease: "easeInOut" },
+            }}
+            onClick={() => setIsMenuOpen(true)}
+            className={styles.menu}
+          ></motion.div>
+        ) : (
+          <div className={styles.menuClose}>
+            <VscClose
+              color="#000000"
+              size={32}
+              onClick={() => setIsMenuOpen(false)}
+            />
+          </div>
+        )
+      ) : null}
 
-      {isMenuOpen && (
+      {!isFirstPage && isMenuOpen && (
         <div className={styles.menuMain}>
+          <span onClick={() => router.push("/home")}>HOME</span>
           <span>LOGIN</span>
           <span>CART</span>
           <span>FAQS</span>
